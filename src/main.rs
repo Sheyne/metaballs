e5d18b5dc78950d10f5eebe57917860e726e2d66
Sheyne Anderson <sheyne@sheyne.com>
2021-11-13 08:18:23 +0000

    Pretty trails

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use core::fmt::Debug;
-use num::{integer::sqrt, Num};
+use num::{integer::sqrt, Num, Saturating};
 
 mod endpoint;
 use endpoint::*;
@@ -176,96 +176,163 @@ fn draw_line<const W: usize, const H: usize>(
     }
 }
 
-fn colorize<Num: Copy, F: Fn(Num) -> (u8, u8, u8), const W: usize, const H: usize>(
-    f: F,
-    src: &[[Num; W]; H],
-    result: &mut [[(u8, u8, u8); W]; H],
-) {
-    for j in 0..H {
-        for i in 0..W {
-            result[j][i] = f(src[j][i]);
+fn find_all_lines<
+    'a,
+    N: Num + Copy + PartialOrd,
+    Cvt: Fn(usize) -> N + 'a + Copy,
+    const W: usize,
+    const H: usize,
+>(
+    threshold: N,
+    scale: N,
+    cvt: Cvt,
+    result: &'a [[N; W]; H],
+) -> impl Iterator<Item = Line<N>> + 'a {
+    (0..(H - 1)).into_iter().flat_map(move |y| {
+        (0..(W - 1)).into_iter().flat_map(move |x| {
+            find_contour(
+                threshold,
+                result[y][x],
+                result[y][x + 1],
+                result[y + 1][x],
+                result[y + 1][x + 1],
+            )
+            .lines()
+            .into_iter()
+            .map(
+                move |Line {
+                          a: (x1, y1),
+                          b: (x2, y2),
+                      }| Line {
+                    a: ((x1 + cvt(x)) * scale, (y1 + cvt(y)) * scale),
+                    b: ((x2 + cvt(x)) * scale, (y2 + cvt(y)) * scale),
+                },
+            )
+        })
+    })
+}
+
+struct Blob {
+    center: (f32, f32),
+    velocity: (f32, f32),
+    size: f32,
+}
+
+fn step<'a>((width, height): (f32, f32), blobs: impl Iterator<Item = &'a mut Blob>) {
+    for blob in blobs {
+        blob.center.0 += blob.velocity.0 / 10.0;
+        blob.center.1 += blob.velocity.1 / 10.0;
+
+        let mut extra_step = false;
+        if blob.center.0 + blob.size > width || blob.center.0 - blob.size < 0.0 {
+            blob.velocity.0 = -blob.velocity.0;
+            extra_step = true;
+        }
+        if blob.center.1 + blob.size > height || blob.center.1 - blob.size < 0.0 {
+            blob.velocity.1 = -blob.velocity.1;
+            extra_step = true;
+        }
+        if extra_step {
+            blob.center.0 += blob.velocity.0 / 10.0;
+            blob.center.1 += blob.velocity.1 / 10.0;
         }
     }
 }
 
-fn save_png<const W: usize, const H: usize>(path: &str, img: &[[(u8, u8, u8); W]; H]) {
+fn energy<'a>((x, y): (f32, f32), blobs: impl Iterator<Item = &'a Blob>) -> f32 {
+    blobs
+        .map(
+            |Blob {
+                 center: (cx, cy),
+                 velocity: _,
+                 size,
+             }| { size / f32::sqrt((x - cx) * (x - cx) + (y - cy) * (y - cy)) },
+        )
+        .sum()
+}
+
+fn main() {
+    const WIDTH: usize = 50;
+    const HEIGHT: usize = 50;
+    const SCALE: usize = 10;
+    const NUM_FRAMES: usize = 1000;
+
     // For reading and opening files
     use std::fs::File;
     use std::io::BufWriter;
     use std::path::Path;
 
-    let path = Path::new(path);
+    let path = Path::new("result.png");
     let file = File::create(path).unwrap();
     let w = &mut BufWriter::new(file);
 
-    let mut encoder = png::Encoder::new(w, W as u32, H as u32); // Width is 2 pixels and height is 1.
+    let mut encoder = png::Encoder::new(w, (WIDTH * SCALE) as u32, (HEIGHT * SCALE) as u32);
     encoder.set_color(png::ColorType::Rgb);
     encoder.set_depth(png::BitDepth::Eight);
+    encoder.set_animated(NUM_FRAMES as u32, 0).unwrap();
 
     let mut writer = encoder.write_header().unwrap();
 
-    let res: Vec<u8> = img
-        .iter()
-        .flat_map(|r| r.iter().flat_map(|(r, g, b)| [*r, *g, *b]))
-        .collect();
-
-    writer.write_image_data(&res).unwrap(); // Save
-}
-
-fn main() {
-    const WIDTH: usize = 10;
-    const HEIGHT: usize = 10;
-    const SCALE: usize = 25;
-
-    let mut result = [[0f32; WIDTH]; HEIGHT];
-    let mut big_result = [[0f32; WIDTH * SCALE]; HEIGHT * SCALE];
     let mut img = [[(0xff, 0xff, 0xff); WIDTH * SCALE]; HEIGHT * SCALE];
 
-    let f = |x, y| ((x - 5f32) * (x - 5f32) + (y - 5f32) * (y - 5f32)) / 10f32 * 10f32;
-    let x_rng = (F32(0f32), F32(10f32));
-    let y_rng = (F32(0f32), F32(10f32));
+    let mut blobs = vec![
+        Blob {
+            center: (1.0, 1.0),
+            velocity: (1.0, 0.7),
+            size: f32::sqrt(0.6),
+        },
+        Blob {
+            center: (4.0, 6.0),
+            velocity: (-2.0, 1.0),
+            size: f32::sqrt(0.3),
+        },
+        Blob {
+            center: (6.0, 2.0),
+            velocity: (-0.7, -0.2),
+            size: f32::sqrt(0.4),
+        },
+        Blob {
+            center: (8.0, 4.0),
+            velocity: (0.4, -1.4),
+            size: f32::sqrt(0.1),
+        },
+    ];
 
-    sample_points(f, x_rng, y_rng, &mut big_result);
+    for _ in 0..NUM_FRAMES {
+        img.iter_mut().flatten().for_each(|(r, g, b)| {
+            *r = r.saturating_add(40);
+            *g = g.saturating_add(40);
+            *b = b.saturating_add(40);
+        });
 
-    colorize(|x| (0, 0xbb, (x * 10f32) as u8), &big_result, &mut img);
+        let mut result = [[0f32; WIDTH]; HEIGHT];
+        let mut img_data = [[0f32; WIDTH * SCALE]; HEIGHT * SCALE];
 
-    sample_points(f, x_rng, y_rng, &mut result);
+        let f = |x, y| energy((x, y), blobs.iter());
+        let x_rng = (F32(0f32), F32(10f32));
+        let y_rng = (F32(0f32), F32(10f32));
 
-    for y in 0..(HEIGHT - 1) {
-        for x in 0..(WIDTH - 1) {
-            let contour = find_contour(
-                10f32,
-                result[y][x],
-                result[y][x + 1],
-                result[y + 1][x],
-                result[y + 1][x + 1],
-            );
-            for Line {
-                a: (x1, y1),
-                b: (x2, y2),
-            } in contour.lines().into_iter().map(
-                |Line {
-                     a: (x1, y1),
-                     b: (x2, y2),
-                 }| Line {
-                    a: (
-                        ((x1 + x as f32) * SCALE as f32) as usize,
-                        ((y1 + y as f32) * SCALE as f32) as usize,
-                    ),
-                    b: (
-                        ((x2 + x as f32) * SCALE as f32) as usize,
-                        ((y2 + y as f32) * SCALE as f32) as usize,
-                    ),
-                },
-            ) {
-                draw_line(
-                    (0xff, 0, 0),
-                    (x1 as i32, y1 as i32),
-                    (x2 as i32, y2 as i32),
-                    &mut img,
-                );
-                save_png("result.png", &img);
-            }
+        sample_points(f, x_rng, y_rng, &mut img_data);
+
+        sample_points(f, x_rng, y_rng, &mut result);
+
+        for line in find_all_lines(1.0, SCALE as f32, |x| x as f32, &result) {
+            let x1 = line.a.0 as i32;
+            let y1 = line.a.1 as i32;
+            let x2 = line.b.0 as i32;
+            let y2 = line.b.1 as i32;
+
+            draw_line((0, 0, 0xff), (x1, y1), (x2, y2), &mut img);
         }
+
+        step((10.0, 10.0), blobs.iter_mut());
+
+        let ptr: *const u8 = unsafe { core::mem::transmute(img.as_ptr()) };
+
+        writer
+            .write_image_data(unsafe {
+                std::slice::from_raw_parts(ptr, WIDTH * HEIGHT * SCALE * SCALE * 3)
+            })
+            .unwrap(); // Save
     }
 }
